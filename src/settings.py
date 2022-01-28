fa4f07fb2e59bd57e287cf0d4d89bbbb6bae7e66
Xia <tiancheng.xia2001@gmail.com>
2022-01-28 12:51:30 +0100

    Settings tweak

--- src/settings.py
+++ src/settings.py
@@ -5,9 +5,9 @@ BACKGROUND_COLOR = (255, 255, 255)
 # Grid settings
 WIDTH, HEIGHT = 10, 20
 CELL_SIZE = 25
-BORDER_COLOR = (0, 0, 0)
+BORDER_COLOR = (50, 50, 50)
 EMPTY_COLOR = (255, 255, 255)
 
 
 # Game settings
-FPS = 20
+FPS = 10
